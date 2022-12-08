7f12f81f1b4884e8faca4c6bc6203937458bf0c8
eoinq <79084155+eoinq@users.noreply.github.com>
2022-12-08 22:50:18 +0100

    Update secretsanta.py

--- secretsanta.py
+++ secretsanta.py
@@ -90,9 +90,10 @@ def retrieve_pairing(retrieve_year):
     try:
         pairing = pickle.load( open(f"previous/pairs_{retrieve_year}.dat", "rb" ) )
         for (giver, receiver) in pairing:
-            print(f"{giver} -> {receiver}")
-        except:
+            print(f"{giver['name']} -> {receiver['name']}")
+    except:
         print(f"No file found file for {retrieve_year}")
+
 
 
 class Mail():
